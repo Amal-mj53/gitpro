296e965c819360f1282106fac7593592f954ab58
Amal <am112mj@gmail.com>
2026-06-30 01:48:51 +0300

    first file Scanner

---
+++ A.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Amal{
+public static void main(String []args){
+Scanner in=new Scanner(System.in);
+System.out.println(" enter name");
+String a=in.next();
+System.out.println("your name is " + a);
+}
+}
+
+
